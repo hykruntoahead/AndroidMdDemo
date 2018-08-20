e34950daec5a65a629d9b676897eb82ffce61f67
Administrator <heyukun@iaijian.com>
2018-08-20 16:44:51 +0800

    FloatingActionButton

--- app/src/main/java/com/iaijian/robot/androidmddemo/MainActivity.java
+++ app/src/main/java/com/iaijian/robot/androidmddemo/MainActivity.java
@@ -3,7 +3,9 @@ package com.iaijian.robot.androidmddemo;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -12,6 +14,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
-//        navigationView.setCheckedItem(R.id.nav_call);
         navigationView.setNavigationItemSelectedListener(new NavigationView
                 .OnNavigationItemSelectedListener() {
             @Override
@@ -44,6 +46,22 @@ public class MainActivity extends AppCompatActivity {
                 navigationView.setCheckedItem(item.getItemId());
                 mDrawerLayout.closeDrawers();
                 return false;
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"FAB Clicked",Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(getApplicationContext(),"FAB restored",Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        })
+                        .show();
             }
         });
     }
