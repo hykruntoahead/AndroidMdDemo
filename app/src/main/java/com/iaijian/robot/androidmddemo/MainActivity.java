69f33d17fd49ab2a77758127fbf68f56aafa90e7
Administrator <heyukun@iaijian.com>
2018-08-20 16:28:58 +0800

    navigation view

--- app/src/main/java/com/iaijian/robot/androidmddemo/MainActivity.java
+++ app/src/main/java/com/iaijian/robot/androidmddemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.iaijian.robot.androidmddemo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -27,11 +29,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mDrawerLayout = findViewById(R.id.drawer_layout);
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+//        navigationView.setCheckedItem(R.id.nav_call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView
+                .OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                navigationView.setCheckedItem(item.getItemId());
+                mDrawerLayout.closeDrawers();
+                return false;
+            }
+        });
     }
 
     @Override
